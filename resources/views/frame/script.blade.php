a2a41facd65234debafdc8c5ab7b6e627f0e84a5
afiqrzli2022 <136997896+afiqrzli2022@users.noreply.github.com>
2023-10-19 19:50:34 +0800

    Htaccess with asset

--- resources/views/frame/script.blade.php
+++ resources/views/frame/script.blade.php
@@ -1,6 +1,6 @@
-<script src="{{ secure_asset('bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ secure_asset('js/startup-modern.js') }}"></script>
-<script src="{{ secure_asset('js/education-level.js') }}"></script>
+<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/startup-modern.js') }}"></script>
+<script src="{{ asset('js/education-level.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 @if(session('success'))
     <script>alert("{{ session('success') }}");</script>
