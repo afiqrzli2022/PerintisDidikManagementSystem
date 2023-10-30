e5f48bf9517fb137c6c7767ef8282386ffa02055
afiqrzli2022 <136997896+afiqrzli2022@users.noreply.github.com>
2023-10-30 16:01:47 +0800

    Fix error test 5

--- resources/views/frame/script.blade.php
+++ resources/views/frame/script.blade.php
@@ -4,3 +4,10 @@
 <script src="{{ secure_asset('js/package.js') }}"></script>
 <script src="{{ secure_asset('js/subject.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<h1></h1>
+@if(session('success'))
+    <script>alert("{{ session('success') }}");</script>
+@endif
+@if(session('error'))
+    <script>alert("{{ session('error') }}");</script>
+@endif
