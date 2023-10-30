23eb6037be371b6df1abd6a001d146e404a6ff61
afiqrzli2022 <136997896+afiqrzli2022@users.noreply.github.com>
2023-10-30 15:58:40 +0800

    Fix error test 3

--- resources/views/frame/script.blade.php
+++ resources/views/frame/script.blade.php
@@ -1,12 +1,6 @@
-<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/startup-modern.js') }}"></script>
-<script src="{{ asset('js/education-level.js') }}"></script>
-<script src="{{ asset('js/package.js') }}"></script>
-<script src="{{ asset('js/subject.js') }}"></script>
+<script src="{{ secure_asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ secure_asset('js/startup-modern.js') }}"></script>
+<script src="{{ secure_asset('js/education-level.js') }}"></script>
+<script src="{{ secure_asset('js/package.js') }}"></script>
+<script src="{{ secure_asset('js/subject.js') }}"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-@if(session('success'))
-    <script>alert("{{ session('success') }}");</script>
-@endif
-@if(session('error'))
-    <script>alert("{{ session('error') }}");</script>
-@endif
