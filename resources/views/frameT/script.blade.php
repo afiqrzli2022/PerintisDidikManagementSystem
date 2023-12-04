adc9c65c2c70f1d0ff22e73c5b42bc13052e4640
afiqrzli2022 <136997896+afiqrzli2022@users.noreply.github.com>
2023-12-05 05:43:26 +0800

    Update

---
+++ resources/views/frameT/script.blade.php
@@ -0,0 +1,12 @@
+<script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/startup-modern.js') }}"></script>
+<script src="{{ asset('js/package.js') }}"></script>
+<script src="{{ asset('js/subject.js') }}"></script>
+<script src="{{ asset('js/education-level.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+@if(session('success'))
+    <script>alert("{{ session('success') }}");</script>
+@endif
+@if(session('error'))
+    <script>alert("{{ session('error') }}");</script>
+@endif
